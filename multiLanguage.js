c0b996fbec51916c8a0ec76d644f076fe106962f
Dimyvm <69921998+Dimyvm@users.noreply.github.com>
2022-02-15 22:33:45 +0100

    Update multiLanguage.js

--- multiLanguage.js
+++ multiLanguage.js
@@ -10,7 +10,7 @@ function getLanguage() {
         var lang = localStorage.getItem("language");
         var doc = json;
         if (doc != null) {
-          console.log(doc);
+          console.log(doc[lang]);
             alert('Data is loaded ! -> console');
         }
         else{
