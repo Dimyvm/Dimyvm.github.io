d650c9257ba4bcda88ea76e8db954afbf87e762c
Dimyvm <69921998+Dimyvm@users.noreply.github.com>
2022-02-15 16:09:10 +0100

    Update multiLanguage.js

--- multiLanguage.js
+++ multiLanguage.js
@@ -10,7 +10,7 @@ function getLanguage() {
         var lang = localStorage.getItem("language");
         var doc = json;
         if (doc != null) {
-            alert('Data is loaded !' + 'Taal:' + doc.toString);
+            alert('Data is loaded !' + 'Taal:' + doc['en']);
         }
     });
 }
