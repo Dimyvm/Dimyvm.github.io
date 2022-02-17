c4778519c1c64e37bad77dcfe349c0c3b11bd4cc
Dimyvm <69921998+Dimyvm@users.noreply.github.com>
2022-02-17 15:15:40 +0100

    Update multiLanguage.js

--- multiLanguage.js
+++ multiLanguage.js
@@ -1,23 +1,13 @@
 
+var resevedJsondata;
 
-var language;
 function getLanguage() {
-    $.getJSON("languages.json", {format: "json"}, function (json) {
-    
-        (localStorage.getItem('language') == null) ? setLanguage('fr') : false;
+    $.getJSON("languages.json", {format: "json"}, function (jsondata) {
+        (localStorage.getItem('language') == null) ? localStorage.setItem('language', 'en') : false;
 
-        var lang = localStorage.getItem("language");
-        var doc = json;
-        if (doc != null) {
-            
-         var selectedLanguage = doc[lang];
-            Object.keys(selectedLanguage ).forEach(function(key) {
-                console.log( selectedLanguage[key]);
-                document.getElementById(key).textContent = selectedLanguage[key];
-            });
-          
-//           document.getElementById("About me title").textContent = doc[lang]["About me title"];
-        
+        if (jsondata != null) {
+            resevedJsondata = jsondata;
+            pushJsonToHtml();
         }
         else{
             alert('No data is loaded!');
@@ -27,5 +17,15 @@ function getLanguage() {
 
 function setLanguage(lang) {
     localStorage.setItem('language', lang);
-    alert(lang.toString());
+    pushJsonToHtml();
+}
+
+function pushJsonToHtml(){
+
+    var lang = localStorage.getItem("language");
+    var selectedLanguageData = resevedJsondata[lang];
+
+    Object.keys(selectedLanguageData).forEach(function(key) {
+        document.getElementById(key).textContent = data[key];
+    });
 }
